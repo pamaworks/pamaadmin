50193fd8d5cb21458a07e8c9722b72aad0ebb117
hyokwon.park <hyokwon.park@cloudci.co.kr>
2022-01-02 08:59:10 +0900

    add

--- src/main/java/kr/co/cloudci/pamaadmin/PamaAdminApplication.java
+++ src/main/java/kr/co/cloudci/pamaadmin/PamaAdminApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class ParaAdminApplication {
+public class PamaAdminApplication {
 
 
 
     public static void main(String[] args) {
-        SpringApplication.run(ParaAdminApplication.class, args);
+        SpringApplication.run(PamaAdminApplication.class, args);
     }
 
 }
